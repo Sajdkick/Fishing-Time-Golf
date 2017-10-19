596d00792b9cdd5ff1194c6dd8b5443280aa768b
Pontus Björkberg <pontus@crunch-studios.com>
2017-10-19 21:53:39 +0200

    First commit

---
+++ Assets/AdvancedRasterMap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Mapbox.Map;
+using Mapbox.Unity;
+using Mapbox.Utils;
+
+public class AdvancedRasterMap : MonoBehaviour
+{
+	void Start()
+	{
+
+		var pngRasterTile = new RawPngRasterTile();
+
+		//16, 10473, 25333 This works, looks like the top corner of africa?
+		UnwrappedTileId tile = new UnwrappedTileId (16, (int)(200 * 1f), (int)(300 * 1f));
+		var mapID = "mapbox.dark";
+
+		pngRasterTile.Initialize(MapboxAccess.Instance, tile.Canonical, mapID, () =>
+			{
+				if (pngRasterTile.HasError)
+					return;
+
+				var tileQuad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+				tileQuad.transform.SetParent(transform);
+				tileQuad.name = tile.Canonical.ToString();
+				tileQuad.transform.position = new Vector3(0, 0, 0);
+				var texture = new Texture2D(0, 0);
+				texture.LoadImage(pngRasterTile.Data);
+				var material = new Material(Shader.Find("Unlit/Texture"));
+				material.mainTexture = texture;
+				tileQuad.GetComponent<MeshRenderer>().sharedMaterial = material;
+			});
+
+	}
+}
