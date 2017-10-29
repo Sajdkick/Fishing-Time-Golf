ca986ad1f3d312412dd6b0094bde2bb7fa76202f
Pontus Björkberg <pontus.bjorkberg@hotmail.com>
2017-10-29 12:59:58 +0100

    The compass now correctly positions on the screen

--- Assets/Compass.cs
+++ Assets/Compass.cs
@@ -6,7 +6,9 @@ public class Compass : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+        transform.position = Camera.main.ViewportToWorldPoint(new Vector3(0.8f, 0.8f, 2));
+
 	}
 	
 	// Update is called once per frame
